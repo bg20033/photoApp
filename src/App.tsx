0c424cd1b7de42ad54f9551edfcf37943120115b
Bardh Gashi <bardhgashi@Bardhs-MacBook-Air.local>
2026-03-12 22:53:20 +0100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,8 @@ const SharedVideo = lazy(() => import("@/pages/landing/SharedVideo"));
 const SharedPhotos = lazy(() => import("@/pages/landing/SharedPhotos"));
 const Login = lazy(() => import("@/pages/Login"));
 const Landing = lazy(() => import("@/pages/landing/Landing"));
-const ManageClientsIndex = lazy(
-  () => import("@/pages/dashboard/user/ManageClients/Index"),
-);
-const ManageUsersIndex = lazy(
-  () => import("@/pages/dashboard/admin/manageUsers/Index"),
-);
+const ManageClientsIndex = lazy(() => import("@/pages/dashboard/user/ManageClients/Index"));
+const ManageUsersIndex = lazy(() => import("@/pages/dashboard/admin/manageUsers/Index"));
 const EventPlanner = lazy(() => import("@/pages/dashboard/user/EventPlanner"));
 const PlansAndServices = lazy(() => import("@/pages/dashboard/user/PlansAndServices"));
 const ManageTeams = lazy(() => import("@/pages/dashboard/user/ManageTeams/ManageTeams"));
